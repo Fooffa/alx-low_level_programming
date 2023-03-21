f7bec3564052bc403a96a032667fc01576cb9de9
Fooffa <afaf.irkmane@gmail.com>
2023-03-21 10:31:24 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
 * Return: 1 and prints + if number is greater than zero
 *: 0 and prints 0 if number is zero
 *: -1 and prints - if number is less than zero
-* @n: the code ascii of the character
+* @n: the argument of the function
 */
 int print_sign(int n)
 {
@@ -23,4 +23,5 @@ else if (n == 0)
 _putchar(48);
 return (0);
 }
+return (0);
 }
