34a919757e835efb331fd316c72ef6f8fe0d6f13
Fooffa <afaf.irkmane@gmail.com>
2023-03-21 16:13:46 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,18 @@ void print_to_98(int n)
 int i;
 if (n == 98)
 printf("%d\n", n);
+else
+{
 if (n < 98)
 {
-for (i = n; i <= 98; i++)
+for (i = n; i < 98; i++)
 printf("%d, ", i);
 }
 if (n > 98)
 {
-for (i = n; i >= 98; i--)
+for (i = n; i > 98; i--)
 printf("%d, ", i);
+}
+printf("%d\n", 98);
 }
 }
