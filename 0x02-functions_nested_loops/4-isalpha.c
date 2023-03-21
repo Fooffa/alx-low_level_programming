62f9710af934e9a8920b2aa8f78e82aa24222bcc
Fooffa <afaf.irkmane@gmail.com>
2023-03-21 09:10:32 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include"main.h"
+/**
+* _isalpha - checks for character
+* Return: 1 if c is an alphabet. 0 if not
+* @c: the code ascii of the character
+*/
+int _isalpha(int c)
+{
+if ((i >= 97 && i <= 122) || (i >= 65 && i <= 90))
+return (1);
+else
+return (0);
+}
