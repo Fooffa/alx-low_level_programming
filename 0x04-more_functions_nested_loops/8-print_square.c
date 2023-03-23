76c6344ff2fa915465183ea3b4341bd251045a7f
Fooffa <afaf.irkmane@gmail.com>
2023-03-23 15:23:46 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_squar - prints a square
+* print_square -  prints a square
 *@size : input as an integer
 */
 
@@ -9,13 +9,13 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 	_putchar('\n');
 	else
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < size; j++)
 		{
-			for (i = 0; i < n; i++)
+			for (i = 0; i < size; i++)
 				_putchar(35);
 			_putchar('\n');
 		}
