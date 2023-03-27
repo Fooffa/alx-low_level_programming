f7f05bb28a6455dd3e7283150450a84b00389648
Fooffa <afaf.irkmane@gmail.com>
2023-03-27 15:33:04 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*puts2 - prints every other character of a string
+*@str: input as a string
+*/
+
+void puts2(char *str)
+{
+	len = 0, i;
+	while (*(str + len))
+		len = len + 1;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
