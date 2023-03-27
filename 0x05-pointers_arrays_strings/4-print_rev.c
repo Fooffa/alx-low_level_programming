fc05eb62e10c538d1e2494652f8478ad2babccc8
Fooffa <afaf.irkmane@gmail.com>
2023-03-27 13:36:57 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_rev -  prints a string, in reverse, followed by a new line
+*@s : input as a string
+*/
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i = 0;
+
+	while (*(s + len))
+		len = len + 1;
+
+	for (i = len ; i >= 0 ; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
