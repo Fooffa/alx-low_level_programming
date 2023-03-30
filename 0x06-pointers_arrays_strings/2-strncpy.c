ea7a8f041fc3084a048d637a84601e401b525e8d
Fooffa <afaf.irkmane@gmail.com>
2023-03-29 17:19:23 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; j < n; j++)
-	dest[j] = src[j];
+	for (j = 0; j < n && *(src + j); j++)
+		dest[j] = src[j];
+	for (; j < n; j++)
+		dest[j] = '\0';
 
 	return (dest);
 }
