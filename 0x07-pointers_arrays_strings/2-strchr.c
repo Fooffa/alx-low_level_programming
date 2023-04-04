27e5d76f75959a2237b604f29de9c4efa25daa18
Fooffa <afaf.irkmane@gmail.com>
2023-04-04 15:47:38 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string.
+* Return: pointer to the memory area s
+*@s: input as a pointer
+*@c:input as a character
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	return (NULL);
+}
