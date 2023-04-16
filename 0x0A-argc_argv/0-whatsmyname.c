97f70e9e787cb20afbb00995c2fc1a5024e7dcf5
Fooffa <afaf.irkmane@gmail.com>
2023-04-16 15:00:56 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+*main - prints the program's name
+* Return: always 0
+* @argc : argument count
+* @argv : argument vector
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
