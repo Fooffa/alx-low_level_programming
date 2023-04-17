1e9a1cc8b915f7af0d3beb05db5f13b8248ac7dc
Fooffa <afaf.irkmane@gmail.com>
2023-04-17 16:55:13 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's informations
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
