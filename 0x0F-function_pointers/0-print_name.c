8325a88b701d45db2e26fe74d2ba0b9f4bb0565f
Fooffa <afaf.irkmane@gmail.com>
2023-04-19 15:40:11 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+*print_name -  prints a name.
+*@name:input as a char
+*@f: input as a function pointer
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
