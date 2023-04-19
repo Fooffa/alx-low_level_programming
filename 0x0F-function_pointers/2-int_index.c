527e7a221ce3dee1fefcb1ee30560c84dd02ff8c
Fooffa <afaf.irkmane@gmail.com>
2023-04-19 16:25:34 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,9 +1,12 @@
 #include "function_pointers.h"
 
 /**
-*print_name -  prints a name.
-*@name:input as a char
-*@f: input as a function pointer
+*int_index -  searches for an integer
+*Return: the index of the first element for which
+*the cmp function does not return 0
+*@array:input as a array
+*@cmp: input as a function pointer
+*@size: input as an input
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -11,13 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return (-1) ;
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-				return (i);
+			return (i);
 	}
 	return (-1);
 }
