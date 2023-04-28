4b2d17959e4060331bddd23a2deaa2e28b43dcfb
Fooffa <afaf.irkmane@gmail.com>
2023-04-27 19:23:27 -0700

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+*free_list -   frees a list_t list.
+*@head:linked list
+*/
+
+void free_list(list_t *head)
+{
+	list_t *l;
+
+	if (!head)
+		return;
+	while (head->next)
+	{
+		free(head->str);
+		l = head->next;
+		free(head);
+		head = l;
+	}
+
+}
