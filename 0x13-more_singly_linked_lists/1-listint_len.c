2fd41ec759f68b921b31d1c414c90bd9c2df844c
Fooffa <afaf.irkmane@gmail.com>
2023-05-02 03:03:16 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+*listint_len -   the number of elements in a linked list_t list.
+*@h:pointer to the structer
+*Return: the number of elements in a linked list_t list.
+*
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
