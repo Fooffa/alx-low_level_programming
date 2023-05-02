2e49d0431db16bb172753b6d75208ebf2cf123c7
Fooffa <afaf.irkmane@gmail.com>
2023-05-02 02:58:12 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
+
 /**
-*print_listint -  prints all the elements of a list_t list.
+*print_listint -  prints all the elements of a listint_t list.
 *@h:pointer to the structer
 *Return: the number of nodes
-*
 */
 
 size_t print_listint(const listint_t *h)
