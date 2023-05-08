e3b5ffc123af0f101c05b53491fab0c66979e3ec
Fooffa <afaf.irkmane@gmail.com>
2023-05-08 16:51:31 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*get_bit - returns the value of a bit at an index in a decimal number
-*@n: number to search
-*@index: index of the bit
-*Return: value of the bit
+*read_textfile - reads a text file and prints it to the POSIX standard output.
+*@filename: the name of the text file
+*@letters: the number of letters it should read and print
+*Return:  the actual number of letters it could read and print
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(letters * sizeof (char));
+	buf = malloc(letters * sizeof(char));
 	let_rd = read(fd, buf, letters);
 	wr = write(STDOUT_FILENO, buf, let_rd);
 	close(fd);
