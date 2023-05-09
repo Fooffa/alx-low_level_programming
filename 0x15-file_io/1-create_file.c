f2f4412463196c35a4212018958bf5bbf70ba453
Fooffa <afaf.irkmane@gmail.com>
2023-05-09 08:43:29 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+
 	while (text_content[len] != '\0')
 		len++;
 	wr = write(fd, text_content, len);
