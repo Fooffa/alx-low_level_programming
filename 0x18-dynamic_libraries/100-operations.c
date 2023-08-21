8fc8fbeddab6c260295d075211da9e1bf458c7d7
Fooffa <afaf.irkmane@gmail.com>
2023-08-21 16:26:12 -0700

    aks

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,29 +1,30 @@
-#include <stdio.h>
+#include "oprations.h"
+
 int add(int a, int b)
 {
-	return a + b;
+	return (a + b);
 }
 
 int sub(int a, int b)
 {
-	return a - b;
+	return (a - b);
 }
 
 int multi(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
 
 int div(int a, int b)
 {
-	if ( b == 0)
+	if (b == 0)
 		printf("Error");
-	return a / b;
+	return (a / b);
 }
 
 int mod(int a, int b)
 {
-	if ( b == 0)
+	if (b == 0)
 		printf("Error");
-	return a % b;
+	return (a % b);
 }
---
+++ 0x18-dynamic_libraries/oprations.h
@@ -0,0 +1,13 @@
+#ifndef oprations_H
+#define oprations_H
+
+#include <stdio.h>
+
+int add (int a, int b);
+int sub(int a, int b);
+int multi(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+
+#endif
